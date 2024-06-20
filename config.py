08c6d5db8059e0069a9dbf9757921f68bf32f87c
shashank bafna <shashgibbs88@gmail.com>
2024-06-21 01:17:48 +0530

    Boilerplate

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+    DEBUG = os.environ.get('FLASK_DEBUG', 'False') == 'True'
     SELFSERVICE_TYPE = os.environ.get('SELFSERVICE_TYPE', 'MASTER')
     LOG_FILE = os.environ.get('LOG_FILE', 'selfservice.log')
     BITBUCKET_API_BASE_URL = 'https://api.bitbucket.org/2.0/repositories/YOUR_REPO_NAME'
